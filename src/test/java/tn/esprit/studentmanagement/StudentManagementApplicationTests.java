c5a629addade2e93a06d5f7a4f570ed2259cb42d
benabdallahaziz <benabdallah.mohamedaziz@esprit.tn>
2026-02-27 02:50:37 +0100

    add test

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,16 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // This tells Spring to use application-test.properties
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test will now use H2 database instead of MySQL
+        // No changes needed to the test itself!
     }
 
 }
